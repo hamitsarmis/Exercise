38827974c679a6709bd6c1fd5a548310591605c8
hamitsarmis <hamit@hamitsarmis.com>
2023-03-12 15:00:46 +0100

    For development don't use Https Redirection to get rid of development certification registration on linux

--- src/PublicApi/Program.cs
+++ src/PublicApi/Program.cs
@@ -51,8 +51,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else 
+    app.UseHttpsRedirection();
 
 
 app.UseAuthentication();
